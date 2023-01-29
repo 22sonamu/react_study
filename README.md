04aa5b4f155c352be39aaa7167b926d0cdb6f028
22sonamu <22sonamu@naver.com>
2023-01-29 15:37:44 +0900

    2-2강 : props

--- README.md
+++ README.md
@@ -116,3 +116,46 @@ function App() {
 export default App;
 
 ```
+
+- ## 2-2강 : Prop
+```javascript
+import logo from './logo.svg';
+import React, { useState } from 'react';
+import './App.css'; 
+
+//props
+function Header(props){
+  //props에는 Object(title : REACT가 들어있다.) 
+  //props.title 로 REACT 라는 값에 접근할수있다.
+  return <header>
+    <h1><a href='/'>{ props.title }</a></h1>
+  </header>
+}
+function Nav(props){
+  const lis = []
+  for (let i = 0 ; i<props.topics.length; i ++){
+    let t = props.topics[i];
+    //li 는 고유한 KEY 를 가지고 있어야함
+    lis.push(<li key={t.id}><a href={'/read/' + t.id}>{t.title}</a></li>)
+  }
+  return <nav>
+    <ol>
+      { lis }
+    </ol>
+  </nav>
+}
+function App() {
+  const topics = [
+    {id : 1, title : 'html', body:'html is....'},
+    {id : 2, title : 'css', body:'css is....'}
+  ];
+  return (  
+    <div className="App">
+      <Header title = "REACT"></Header>
+      <Nav topics={ topics }></Nav>
+    </div>
+  );
+}
+export default App;
+
+```
--- src/App.js
+++ src/App.js
@@ -2,25 +2,36 @@ import logo from './logo.svg';
 import React, { useState } from 'react';
 import './App.css'; 
 
-//사용자 정의 태그
-function Header(){
+//props
+function Header(props){
+  //props에는 Object(title : REACT가 들어있다.) 
+  //props.title 로 REACT 라는 값에 접근할수있다.
   return <header>
-    <h1><a href='/'>WEB</a></h1>
+    <h1><a href='/'>{ props.title }</a></h1>
   </header>
 }
-function Nav(){
+function Nav(props){
+  const lis = []
+  for (let i = 0 ; i<props.topics.length; i ++){
+    let t = props.topics[i];
+    //li 는 고유한 KEY 를 가지고 있어야함
+    lis.push(<li key={t.id}><a href={'/read/' + t.id}>{t.title}</a></li>)
+  }
   return <nav>
     <ol>
-      <li><a href='/read/1'>html</a></li>
-      <li><a href='/read/2'>css</a></li>
+      { lis }
     </ol>
   </nav>
 }
 function App() {
+  const topics = [
+    {id : 1, title : 'html', body:'html is....'},
+    {id : 2, title : 'css', body:'css is....'}
+  ];
   return (  
     <div className="App">
-      <Header></Header>
-      <Nav></Nav>
+      <Header title = "REACT"></Header>
+      <Nav topics={ topics }></Nav>
     </div>
   );
 }
