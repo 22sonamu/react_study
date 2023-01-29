ef584ce9e848d32b809615b233bf6c8dbab0bee9
22sonamu <22sonamu@naver.com>
2023-01-29 15:19:43 +0900

    2-1 강 : 사용자 정의 태그

--- src/App.js
+++ src/App.js
@@ -2,30 +2,32 @@ import logo from './logo.svg';
 import React, { useState } from 'react';
 import './App.css'; 
 
+//사용자 정의 태그
+function Header(){
+  return <header>
+    <h1><a href='/'>WEB</a></h1>
+  </header>
+}
+function Nav(){
+  return <nav>
+    <ol>
+      <li><a href='/read/1'>html</a></li>
+      <li><a href='/read/2'>css</a></li>
+    </ol>
+  </nav>
+}
 function App() {
 
 
   let [a,b] = useState(['남자 코트 추천', '강남 우동 맛집']);
-  //b(a[0] = '여자 코트 추천'은 불가능하다. ['여자 코트 추천' , '강남 우동 맛집']으로 써주어야함)
+  //b(a[0] = '여자 코트 추천'은 불가능하다. ['여자 코트 추천' , '강남 우동 맛집'])
   let [favorite, plusFavorite] = useState(0);
 
 
   return (  
     <div className="App">
-      <div className = "black-nav">
-        <div>개발 Blog</div>
-      </div> 
-      <div className = "list">
-    
-        <h3 onClick={()=>{b(['여자 코트 추천', '강남 우동 맛집'])}}>{ a[0] } <span onClick={()=>{plusFavorite(favorite + 1)}}>따봉버튼</span> { favorite } </h3>
-        <p>2월 17일 발행</p>
-        <hr/>
-      </div>
-      <div className = "list">
-        <h3>{ a[1] }</h3>
-        <p>2월 17일 발행</p>
-        <hr/>
-      </div>
+      <Header></Header>
+      <Nav></Nav>
     </div>
   );
 }
