f34515d361adc1887746d82b13a4ff3e0e586cc9
22sonamu <22sonamu@naver.com>
2023-08-06 03:04:46 +0900

    Expenses 컴포넌트 분리

---
+++ practice/src/components/Expenses.js
@@ -0,0 +1,30 @@
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+export default function () {
+    const expense = [
+        {
+          id: 'e1',
+          title : 'Toilet Paper',
+          amount : 94.12,
+          date : new Date(2021, 1, 11)
+        },
+        {
+          id : 'e2',
+          title : 'New TV',
+          amout : 100.11,
+          date : new Date(2022, 5, 30)
+        },
+        {
+          id : 'e3',
+          title: 'Car Insurance',
+          amout : 20.9,
+          date : new Date(2023, 1, 29)
+        }
+      ]
+    return (
+        <div className="expenses">
+            <ExpenseItem title={expense[0].title} date={expense[0].date} amount={expense[0].amount}></ExpenseItem>
+            <ExpenseItem title={expense[1].title} date={expense[1].date} amount={expense[1].amount}></ExpenseItem>
+        </div>
+    )
+}
