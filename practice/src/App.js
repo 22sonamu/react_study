4ac1daae1f3dce2ba3dd00f04f2f93ced18e3437
22sonamu <22sonamu@naver.com>
2023-08-11 17:30:22 +0900

    expense list 추가

--- practice/src/App.js
+++ practice/src/App.js
@@ -2,16 +2,38 @@
 import Expenses from "./components/Expenses/Expenses";
 import ExpensesFilter from "./components/Filter/ExpensesFilter";
 import NewExpense from "./components/NewExpense/NewExpense";
+import { useState } from "react";
+const DUMMY_EXPENSES= [
+  {
+    id: 'e1',
+    title : 'Toilet Paper',
+    amount : 94.12,
+    date : new Date(2021, 1, 11)
+  },
+  {
+    id : 'e2',
+    title : 'New TV',
+    amout : 100.11,
+    date : new Date(2022, 5, 30)
+  },
+  {
+    id : 'e3',
+    title: 'Car Insurance',
+    amout : 20.9,
+    date : new Date(2023, 1, 29)
+  }
+]
 function App() {
-
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
   const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses]
+    });
   };
   return (
     <div>
       <NewExpense onAddExpense = {addExpenseHandler}/>
-      <Expenses></Expenses>
+      <Expenses expense={expenses}/>
     </div>
   );
 }
--- practice/src/components/Expenses/Expenses.js
+++ practice/src/components/Expenses/Expenses.js
@@ -3,43 +3,18 @@ import "./Expenses.css";
 import Card from "../UI/Card"
 import ExpensesFilter from "../Filter/ExpensesFilter";
 import { useState } from "react";
-export default function Expense() {
+export default function Expense({expense}) {
   const [filteredYear, setFilteredYear] = useState("2020");
-  let filterInfoText = '2019, 2021 & 2022'
+  
 
-  if(filteredYear === '2019'){
-    filterInfoText = '2020, 2021 & 2022';
-  }if(filteredYear === '2020'){ 
-    filterInfoText = '2019, 2021 & 2022';
-  }if(filteredYear === '2021'){
-    filterInfoText = '2019, 2020 & 2022';
-  }if(filteredYear ==='2022'){
-    filterInfoText = '2019, 2020 & 2021';
-  }
-  const filterChangeHandler = (filteredYear) => {
+ 
+  const filterChangeHandler = (expense) => {
     //이 함수가 동작하면, 컴포넌트가 재실행되면서 위의 if문이 동작한다.
-    setFilteredYear(filteredYear);
+    setFilteredYear(prevExpenses => {
+      return [expense, ...prevExpenses]
+    });
   }
-    const expense = [
-        {
-          id: 'e1',
-          title : 'Toilet Paper',
-          amount : 94.12,
-          date : new Date(2021, 1, 11)
-        },
-        {
-          id : 'e2',
-          title : 'New TV',
-          amout : 100.11,
-          date : new Date(2022, 5, 30)
-        },
-        {
-          id : 'e3',
-          title: 'Car Insurance',
-          amout : 20.9,
-          date : new Date(2023, 1, 29)
-        }
-      ]
+  
     return (
         <Card className="expenses">
             <ExpensesFilter selected ={filteredYear} onChangeFilter={filterChangeHandler}></ExpensesFilter>
@@ -47,4 +22,4 @@ export default function Expense() {
             
         </Card>
     )
-}
+ }
